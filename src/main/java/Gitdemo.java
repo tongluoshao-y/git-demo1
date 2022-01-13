9955561e94cfb6e8c13caf4b52bb18506da07bbd
luoshao <luoshaoatguigu.com>
2022-01-13 18:19:35 +0800

    hello github commit~

--- src/main/java/Gitdemo.java
+++ src/main/java/Gitdemo.java
@@ -1,7 +1,7 @@
 public class Gitdemo {
 
     public static void main(String[] args) {
-        System.out.println("hell! Git!");
-
+        System.out.println("hello! Git!");
+        System.out.println("hello! github");
     }
 }
