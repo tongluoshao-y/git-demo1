9cf2d2a3ed94594d303cab34ad220f059229432c
tongluoshao-y <97668532+tongluoshao-y@users.noreply.github.com>
2022-01-13 18:36:23 +0800

    Update Gitdemo.java

--- src/main/java/Gitdemo.java
+++ src/main/java/Gitdemo.java
@@ -3,5 +3,6 @@ public class Gitdemo {
     public static void main(String[] args) {
         System.out.println("hello! Git!");
         System.out.println("hello! github");
+        System.out.println("pull! test");
     }
 }
